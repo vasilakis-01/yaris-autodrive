22450b05a5a538e73b05ba6011a494cda56c0235
Georgios Charitos <sdi2400212@di.uoa.gr>
2025-12-16 17:16:36 +0200

    statick kai moving objects

---
+++ MovStatObject.hpp
@@ -0,0 +1,50 @@
+#ifndef MOVSTATOBJECT_HPP
+#define MOVSTATOBJECT_HPP
+
+#include <WorldObject.hpp>
+
+//static object funtion
+class StaticObject : public WorldObject{
+    public:
+        StaticObject(std::string type, char glyph , Position pos) : WorldObject(type,glyph,pos) {}
+        
+        bool isStatick() const {return true;}
+        bool isMoving() const  {return false;}
+        void update(int tick) {
+            //its statick doesnt move
+        }
+
+};
+//moving object class
+class MovingObject : public WorldObject{
+    protected: 
+        //speed,direction
+        Direction direction;
+        int speed;
+    
+    public:
+        MovingObject(std::string  type, char glyph, Position pos, Direction dir, int s) : WorldObject(type,glyph,pos),direction(dir), speed(s) {}
+
+        //move to each dir function
+        void update(int tick) {
+            Position movePos = directionToVector(direction);
+            //calculate new position and set it after movement
+            int newX = position.getX() + movePos.getX()*speed;
+            int newY = position.getY() + movePos.getY()*speed;
+            position = Position(newX,newY);
+        }
+
+        bool isStatic() const override { return false; }
+        bool isMoving() const override { return true; }
+    
+        //getters
+        Direction getDirection() const { return direction; }
+        int getSpeed() const { return speed; }
+    
+        //setters
+        void setDirection(Direction newDir) { direction = newDir; }
+        void setSpeed(int newSpeed) { speed = newSpeed; }
+
+};
+
+#endif 
