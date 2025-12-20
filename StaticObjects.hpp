c56c1a480e187b7e83ba35206defa6d19a7f79d6
Georgios Charitos <sdi2400212@di.uoa.gr>
2025-12-20 17:54:35 +0200

    mikro fix

---
+++ StaticObjects.hpp
@@ -0,0 +1,73 @@
+#ifndef STATICKOBJECTS_HPP
+#define STATICKOBJECTS_HPP
+
+#include "MovStatObject.hpp"
+
+//class parked vehicle
+class StationaryVehicle : public StaticObject{
+    public:
+        //initialize the parked vehicle
+        StationaryVehicle(Position pos): StaticObject("PARKED", 'P', pos){}
+        ~StationaryVehicle(){}  // ΔΙΟΡΘΩΣΗ: ~StationaryVehicle() όχι ~StationaryVehivle
+        void update(int tick) override {
+            (void)tick;
+            //it doesnt move
+        }
+};
+
+class StreetSign : public StaticObject{
+    protected:
+        std::string text;
+    
+    public:
+        StreetSign(Position pos, std::string t, char g): StaticObject("SIGN", g, pos), text(t) {}
+        ~StreetSign(){}
+        void update(int tick) override {(void)tick;}
+
+        std::string getText() const {return text;}
+};
+
+class TrafficLight : public StaticObject {
+    private:
+        int state; //0 for red, 1 for green, 2 for yellow
+        int counter;
+
+    public:
+        TrafficLight(Position pos) : StaticObject("LIGHT", 'R', pos), state(0), counter(0) {}
+        ~TrafficLight() {}
+
+        //update traffic light's state function
+        void update(int tick) override {
+            counter++;
+            if (state == 0 && counter >=4) { //red lasts 4 ticks and becomes green
+                state =1;
+                counter = 0;
+                setGlyph('G');
+
+            } else if (state==1 && counter>=8) { //green lasts 8 ticks and becomes yellow
+                state = 2;
+                counter = 0;
+                setGlyph('Y');
+            } else if (state == 2 && counter >=2) { //yellow lasts 2 ticks and becomes red
+                state = 0;
+                counter = 0;
+                setGlyph('R');
+            }
+            (void)tick;
+        }
+
+        //helper functions
+        bool isRed() const {return state==0;}
+        bool isGreen() const {return state==1;}
+        bool isYellow() const {return state==2;}
+
+        std::string getColor() const {
+            if (isRed()) {
+                return "Red";
+            } else if (isGreen()) {
+                return "Green";
+            } else {return "Yellow";}
+        }
+};
+
+#endif
